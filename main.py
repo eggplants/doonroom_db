02f8f969545df80f1da33393238a2265eb02bac1
eggplants <w10776e8w@yahoo.co.jp>
2020-09-20 14:54:56 +0900

    Output fix

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import os
 from doon_parser.parser import Parser
 from doon_parser.database import DoonDatabase
 from typing import List
+from typing import Callable
 
 
-def get_filepaths(dir) -> List[str]:
-    def ext_num(value):
-        return int(value[-3:].replace('p', '').replace('=', ''))
+def get_filepaths(dir: str) -> List[str]:
+    ext_num: Callable[str] = lambda value: int(
+        value[-3:].replace('p', '').replace('=', ''))
     return sorted(glob(os.path.join('.', dir, '*')), key=ext_num)
 
 
 def main() -> None:
     parsed_data = []
     for category in ('dojin', 'hypno'):
+        print(' ' * 25, end='\r')
         parser = Parser(category)
         for path in get_filepaths(category):
             parsed_data.extend(parser.execute(path))
